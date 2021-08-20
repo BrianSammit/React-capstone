bf85e369cb906a1e76561eb48386f145ebee1033
BrianSammit <briansammit.cruz@gmail.com>
2021-08-19 21:14:47 -0500

    Add principal page layout

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import {
+  AppBar, Grid, Toolbar, Card, CardContent,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Pokedex = () => <div>this is de pokedex page</div>;
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: '20px',
+    paddingLeft: '50px',
+    paddingRigth: '50px',
+  },
+});
+
+const getPokemonCard = () => (
+  <Grid item xs={4}>
+    <Card>
+      <CardContent>HI</CardContent>
+    </Card>
+  </Grid>
+);
+
+const Pokedex = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+      </Grid>
+    </>
+  );
+};
 
 export default Pokedex;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Pokemon = () => <div>this is de pokemon page</div>;
+const Pokemon = (props) => {
+  const { match } = props;
+  const { params } = match;
+  const { pokemonId } = params;
+  return <div>{`this is de pokemon page for pokemon  #${pokemonId}`}</div>;
+};
+
+Pokemon.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  match: PropTypes.any.isRequired,
+};
 
 export default Pokemon;
