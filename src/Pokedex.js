e8a4ddfa089f4e2bc505721f2d5b9ab35b93b15e
BrianSammit <briansammit.cruz@gmail.com>
2021-08-23 14:11:39 -0500

    Add search funtion

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,12 @@
-/* eslint-disable object-curly-newline, no-unused-vars, react/forbid-prop-types, react/prop-types */
+/* eslint-disable object-curly-newline, no-unused-vars, react/forbid-prop-types, react/prop-types, max-len */
 import React, { useState, useEffect } from 'react';
-import { AppBar, Grid, Toolbar, Card, CardContent, CircularProgress, CardMedia, Typography, Input } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Grid, Toolbar, Card, CardContent, CircularProgress, CardMedia, Typography, Input, TextField } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 import axios from 'axios';
 import toFirtCharUpperCase from './constants';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   pokedexContainer: {
     paddingTop: '20px',
     padding: '50px',
@@ -16,12 +17,33 @@ const useStyles = makeStyles({
   CardContent: {
     textAlign: 'center',
   },
-});
+  searchContainer: {
+    display: 'flex',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    marginTop: '5px',
+    marginBottom: '5px',
+  },
+  searchIcon: {
+    alignSelf: 'flex-end',
+    marginBottom: '5px',
+  },
+  searchInput: {
+    width: '200px',
+    margin: '5px',
+  },
+}));
 
 const Pokedex = (props) => {
   const { history } = props;
   const classes = useStyles();
   const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState('');
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=898').then((response) => {
@@ -57,11 +79,16 @@ const Pokedex = (props) => {
   return (
     <>
       <AppBar position="static">
-        <Toolbar />
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField className={classes.searchInput} onChange={handleSearchChange} label="Pokemon" variant="standard" />
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
-          {Object.keys(pokemonData).map((pokemonId) => getPokemonCard(pokemonId))}
+          {Object.keys(pokemonData).map((pokemonId) => pokemonData[pokemonId].name.includes(filter) && getPokemonCard(pokemonId))}
         </Grid>
       ) : (
         <CircularProgress />
@@ -72,4 +99,4 @@ const Pokedex = (props) => {
 
 export default Pokedex;
 
-/* eslint-enable object-curly-newline, no-unused-vars, react/forbid-prop-types */
+/* eslint-enable object-curly-newline, no-unused-vars, react/forbid-prop-types, max-len */
